2638ee541d70902009094dba61e4b70a1e64ca4f
Santa-Mb <118125983+Santa-Mb@users.noreply.github.com>
2022-11-15 22:00:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.vcardowner = ['9472 253 5399'] //ur owner number
+global.ownername = "🦄Queen X-Niki😈" //ur owner name
+global.ytname = "YT: QUEEN NIKI" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.owner = ['9472 253 5399']
+global.ownertag = '9472 253 5399' //ur tag number
+global.botname = 'Queen X-Niki Bot MD V4' //ur bot name
+global.linkz = "https://youtube.com/channel/UC8Ai9UIurYpakkRLLN1_4Ig" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UC8Ai9UIurYpakkRLLN1_4Ig" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "Niki Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "😈Queen Niki Bot MD" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
